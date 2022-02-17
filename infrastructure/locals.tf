dc042895688a423f2e0592932a6f5ce1b39e47d7
Andy Langridge <andy.langridge@kinandcarta.com>
2022-02-17 15:37:27 +0000

    Change resource suffix to include tooling as environment value

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  resource_suffix = "shared-${module.azure_region_primary.location_short}"
+  resource_suffix = "shared-${var.environment}-${module.azure_region_primary.location_short}"
 
   tags = {
     CostCentre  = "90117"
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,3 +1,9 @@
+variable "environment" {
+  description = "The environment resources are deployed to e.g. 'tooling'"
+  type        = string
+  default     = "tooling"
+}
+
 variable "primary_region" {
   description = "The primary region resources are deployed to in slug format e.g. 'uk-south'"
   type        = string
