3856ce0e2a6cc391b16df5e043f439ebfe4394ca
Andy Langridge <andy.langridge@kinandcarta.com>
2022-02-22 18:11:53 +0000

    Enable public access on contianer registry

--- infrastructure/container-registry.tf
+++ infrastructure/container-registry.tf
@@ -1,9 +1,9 @@
 resource "azurerm_container_registry" "acr" {
-  name                          = "pinscr${replace(local.resource_suffix, "-", "")}"
-  resource_group_name           = azurerm_resource_group.tooling.name
-  location                      = azurerm_resource_group.tooling.location
-  public_network_access_enabled = false
-  sku                           = "Premium"
+  #checkov:skip=CKV_AZURE_139: Access over internet required so Azure DevOps can push/pull images
+  name                = "pinscr${replace(local.resource_suffix, "-", "")}"
+  resource_group_name = azurerm_resource_group.tooling.name
+  location            = azurerm_resource_group.tooling.location
+  sku                 = "Premium"
 
   georeplications {
     location = module.azure_region_secondary.location
