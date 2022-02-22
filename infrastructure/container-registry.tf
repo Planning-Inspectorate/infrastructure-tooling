6a824961e90eb71dd29d2752c28a3dc91b128ac2
Andy Langridge <andy.langridg3@gmail.com>
2022-02-22 16:13:43 +0000

    Extend wrapper templates for CI and CD pipeline (#3)
    * Update pipelines to extend wrapper templates
    
    * Disable public network access for container registry
    
    * Remove param for environments
    
    * Change global var format
    
    Co-authored-by: Andy Langridge <andy.langridge@kinandcarta.com>

--- infrastructure/container-registry.tf
+++ infrastructure/container-registry.tf
@@ -1,8 +1,9 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "pinscr${replace(local.resource_suffix, "-", "")}"
-  resource_group_name = azurerm_resource_group.tooling.name
-  location            = azurerm_resource_group.tooling.location
-  sku                 = "Premium"
+  name                          = "pinscr${replace(local.resource_suffix, "-", "")}"
+  resource_group_name           = azurerm_resource_group.tooling.name
+  location                      = azurerm_resource_group.tooling.location
+  public_network_access_enabled = false
+  sku                           = "Premium"
 
   georeplications {
     location = module.azure_region_secondary.location
