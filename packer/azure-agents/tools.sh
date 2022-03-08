76e163a9d5b14e268b95b4991ea2c44757e8b4e3
Andy Langridge <andy.langridge@kinandcarta.com>
2022-03-08 12:07:30 +0000

    Add Docker as tool

--- packer/azure-agents/tools.sh
+++ packer/azure-agents/tools.sh
@@ -6,9 +6,11 @@ sudo echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
 
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
+  ca-certificates \
   curl \
   gnupg \
   jq \
+  lsb-release \
   software-properties-common \
   unzip \
   zip
@@ -26,6 +28,18 @@ sudo apt install -y --no-install-recommends \
 sudo apt install -y --no-install-recommends \
   python3.7 \
   python3-pip
+
+# Docker Engine
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Terraform 1.1.6
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
