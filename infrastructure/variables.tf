d59ce2d7311bcfbfe18e485a4494772759939e8a
Andy Langridge <andy.langridge@kinandcarta.com>
2022-03-22 20:50:18 +0000

    Remove peering resources as they have moved to the environments repo

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,12 +4,6 @@ variable "environment" {
   default     = "tooling"
 }
 
-variable "environment_vnets" {
-  description = "A map containing the VNet name and resource group for each environment"
-  type        = map(map(string))
-  default     = {}
-}
-
 variable "primary_region" {
   description = "The primary region resources are deployed to in slug format e.g. 'uk-south'"
   type        = string
