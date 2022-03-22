680624d356a6f1fbcd0bf51ebac797223c85864c
Andy Langridge <andy.langridg3@gmail.com>
2022-03-22 21:07:33 +0000

    Remove peering resources as they have moved to the environments repo (#19)
    Co-authored-by: Andy Langridge <andy.langridge@kinandcarta.com>

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -5,30 +5,3 @@ data "azurerm_image" "azure_agents" {
   sort_descending     = true
   resource_group_name = azurerm_resource_group.tooling.name
 }
-
-data "azurerm_virtual_network" "dev" {
-  count = can(var.environment_vnets["dev"]) ? 1 : 0
-
-  name                = var.environment_vnets["dev"]["name"]
-  resource_group_name = var.environment_vnets["dev"]["resource_group"]
-
-  provider = azurerm.dev
-}
-
-data "azurerm_virtual_network" "test" {
-  count = can(var.environment_vnets["test"]) ? 1 : 0
-
-  name                = var.environment_vnets["test"]["name"]
-  resource_group_name = var.environment_vnets["test"]["resource_group"]
-
-  provider = azurerm.test
-}
-
-data "azurerm_virtual_network" "prod" {
-  count = can(var.environment_vnets["prod"]) ? 1 : 0
-
-  name                = var.environment_vnets["prod"]["name"]
-  resource_group_name = var.environment_vnets["prod"]["resource_group"]
-
-  provider = azurerm.prod
-}
