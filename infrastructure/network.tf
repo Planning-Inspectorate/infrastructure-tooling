fff29040d8a6085084e4be273ffaeacbead36e4b
Lester March <lestermarch@outlook.com>
2022-04-06 17:06:14 +0100

    Add private DNS zone for database.windows.net

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -35,3 +35,15 @@ resource "azurerm_private_dns_zone_virtual_network_link" "cosmosdb" {
   resource_group_name   = azurerm_resource_group.tooling.name
   virtual_network_id    = azurerm_virtual_network.tooling.id
 }
+
+resource "azurerm_private_dns_zone" "database" {
+  name                = "privatelink.database.windows.net"
+  resource_group_name = azurerm_resource_group.tooling.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "back_office_sql_server" {
+  name                  = "pins-vnetlink-sql-server-${local.resource_suffix}"
+  private_dns_zone_name = azurerm_private_dns_zone.database.name
+  resource_group_name   = azurerm_resource_group.tooling.name
+  virtual_network_id    = azurerm_virtual_network.tooling.id
+}
