d5c1f7a12637cb355ae14897ab8f8eeb82d98d3b
Lester March <91114720+lestermarch@users.noreply.github.com>
2022-06-06 09:47:24 +0100

    Add an agent pool for running tests (#25)
    * Add test agent pool
    
    * Update common pipeline template repo ref version
    
    * Remove project parameter from CD pipeline
    
    * Remove projectName parameter from CI pipeline
    
    * Remove authType deployment stage parameter
    
    * Add container and artifact parameters to deployment stages
    
    * Remove container parameter
    
    * Remove artifact parameter
    
    * Remove comment from agent build pipeline

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -1,4 +1,15 @@
 locals {
+  agent_pools = {
+    main = {
+      name     = "pins-vmss-${local.resource_suffix}"
+      nic_name = "pins-vnet-azure-agents-nic-${local.resource_suffix}"
+    }
+    test = {
+      name     = "pins-vmss-test-${local.resource_suffix}"
+      nic_name = "pins-vnet-azure-agents-nic-test-${local.resource_suffix}"
+    }
+  }
+
   resource_suffix = "shared-${var.environment}-${module.azure_region_primary.location_short}"
 
   tags = {
