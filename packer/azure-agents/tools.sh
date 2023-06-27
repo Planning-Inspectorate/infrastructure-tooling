943ad4923d7f56217e3446122592274918507098
nasir-rahman9 <122629783+nasir-rahman9@users.noreply.github.com>
2023-06-27 19:06:12 +0100

    fix the checkov version to 2.3.240 to resolve CI error (#34)
    * fix the checkov version to 2.3.240 to resolve CI error
    
    * reverted tflint to 0.14.0
    
    * update tflint version
    
    * upgraded tflint to 0.24.0
    
    * pin checkov version to 2.2.94
    
    * revert to ubuntu-server-focal
    
    * updated code to download checkov
    
    * updated code to download checkov

--- packer/azure-agents/tools.sh
+++ packer/azure-agents/tools.sh
@@ -68,7 +68,8 @@ sudo apt-get install -y terraform=1.1.6
 sudo curl -s -L "https://github.com/gruntwork-io/terragrunt/releases/download/v0.36.1/terragrunt_linux_amd64" -o /usr/bin/terragrunt && chmod 777 /usr/bin/terragrunt
 
 # Checkov
-python3.7 -m pip install -U checkov
+python3.7 -m pip install --force-reinstall packaging==21
+python3.7 -m pip install -U checkov==2.2.94
 
 # TFLint
 curl -s https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
