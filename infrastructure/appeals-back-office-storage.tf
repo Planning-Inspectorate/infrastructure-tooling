0e174251793f602e7eb4e70f70784b82feded679
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2024-04-16 15:22:49 +0100

    change length of storage name to under the limit of 24

--- infrastructure/appeals-back-office-storage.tf
+++ infrastructure/appeals-back-office-storage.tf
@@ -4,7 +4,7 @@ resource "azurerm_storage_account" "appeals_back_office_terraform_storage" {
   #checkov:skip=CKV2_AZURE_18: Customer Managed Keys not implemented
   #checkov:skip=CKV_AZURE_33: logging not required
   #checkov:skip=CKV_AZURE_35: terraform requires access
-  name                     = "pinssttfstateuksappealsbackoffice"
+  name                     = "pinssttfstateuksappealbo"
   resource_group_name      = local.shared_terraform_resource_group
   location                 = azurerm_resource_group.tooling.location
   account_tier             = "Standard"
@@ -18,7 +18,7 @@ resource "azurerm_storage_container" "appeals_back_office_terraform_storage_cont
   for_each = toset(["dev", "test", "prod"])
 
   #checkov:skip=CKV2_AZURE_21: logging not required
-  name                  = "appeals-back-office-terraform-state-${each.key}"
+  name                  = "terraform-state-appeals-back-office-${each.key}"
   storage_account_name  = azurerm_storage_account.appeals_back_office_terraform_storage.name
   container_access_type = "private"
 }
