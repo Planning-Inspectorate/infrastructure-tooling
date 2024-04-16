49ac26fbfb4828933650ccd3480b9543960df7d1
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-04-16 11:28:45 +0100

    fix: backend can't use variables

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = local.shared_terraform_resource_group
+    resource_group_name  = "pins-rg-shared-terraform-uks" # can't use the local var for this
     storage_account_name = "pinsstsharedtfstateuks"
     container_name       = "terraformstate"
     key                  = "tooling.tfstate"
