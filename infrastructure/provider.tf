91f0fb886b4b20c829692540f5352e8031f4f1e8
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2024-07-15 08:31:54 +0100

    remove tf version

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,12 +1,7 @@
 terraform {
-  required_version = ">= 1.7.3, <= 1.9.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.0"
-    }
-    random = {
-      source  = "hashicorp/random"
       version = "~> 3.0.0"
     }
   }
