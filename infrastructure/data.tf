1bfc4ab4eea3beb6dbdfcb465c1ce9532bfeea32
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-07-15 11:43:13 +0100

    temp: specific agent image for rollback

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 data "azurerm_image" "azure_agents" {
-  name_regex          = "azure-agents"
+  name_regex          = "azure-agents-20240216"
   sort_descending     = true
   resource_group_name = azurerm_resource_group.tooling.name
 }
