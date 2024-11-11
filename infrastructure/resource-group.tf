db276763437c97a38b1a8ca13a4713c76ff04e3c
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-11-11 15:03:58 +0000

    feat(infrastructure): add shared front door instance

--- infrastructure/resource-group.tf
+++ infrastructure/resource-group.tf
@@ -3,3 +3,9 @@ resource "azurerm_resource_group" "tooling" {
   location = module.azure_region_primary.location
   tags     = local.tags
 }
+
+resource "azurerm_resource_group" "common" {
+  name     = "pins-rg-common-tooling"
+  location = module.azure_region_primary.location
+  tags     = local.tags
+}
