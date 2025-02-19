10d0235939ffd8fd252eb291048f61823924f8a5
Medini Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2025-02-19 16:09:42 +0000

    fix(infrastructure): updating azure agent image name (#104)

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -3,7 +3,7 @@ data "azurerm_client_config" "current" {}
 data "azurerm_subscription" "current" {}
 
 data "azurerm_image" "azure_agents" {
-  name_regex          = "azure-agents-20241223142908"
+  name_regex          = "azure-agents-20241114133024"
   sort_descending     = true
   resource_group_name = azurerm_resource_group.tooling.name
 }
