fb931d19f0344943c97c91dacf553ab49e78067b
Medini Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2025-02-19 15:45:21 +0000

    feat(infrastructure): adding storage account for appeals front office (#101)
    * feat(infrastructure): adding storage account for appeals front office
    
    * feat(infrastructure): updating storage account for appeals front office

---
+++ infrastructure/appeals-front-office-storage.tf
@@ -0,0 +1,25 @@
+
+resource "azurerm_storage_account" "appeals_front_office_terraform_storage" {
+  #checkov:skip=CKV2_AZURE_1: Customer Managed Keys not implemented
+  #checkov:skip=CKV2_AZURE_18: Customer Managed Keys not implemented
+  #checkov:skip=CKV_AZURE_33: logging not required
+  #checkov:skip=CKV_AZURE_35: terraform requires access
+  name                             = "pinssttfstateuksappealfo"
+  resource_group_name              = local.shared_terraform_resource_group
+  location                         = azurerm_resource_group.tooling.location
+  account_tier                     = "Standard"
+  account_replication_type         = "LRS"
+  min_tls_version                  = "TLS1_2"
+  cross_tenant_replication_enabled = true
+
+  tags = local.tags
+}
+
+resource "azurerm_storage_container" "appeals_front_office_terraform_storage_containers" {
+  for_each = toset(["dev", "test", "prod", "training"])
+
+  #checkov:skip=CKV2_AZURE_21: logging not required
+  name                  = "terraform-state-appeals-front-office-${each.key}"
+  storage_account_name  = azurerm_storage_account.appeals_front_office_terraform_storage.name
+  container_access_type = "private"
+}
