788eb2cfccb4ec3a6a22a8c64bc85ac960b7ee55
Medini-Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2025-04-15 14:10:25 +0100

    feat(tooling): adding log analytics and diagnostic setting for front door

--- infrastructure/front-door.tf
+++ infrastructure/front-door.tf
@@ -16,3 +16,37 @@ resource "azurerm_cdn_frontdoor_endpoint" "common" {
 
   tags = local.tags
 }
+
+#front door monitoring log analytics
+resource "azurerm_log_analytics_workspace" "common" {
+  name                = "pins-log-common-tooling"
+  location            = module.azure_region_primary.location
+  resource_group_name = azurerm_resource_group.common.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+
+  tags = local.tags
+}
+
+#front door monitoring - diagnostic setting
+resource "azurerm_monitor_diagnostic_setting" "web_front_door" {
+  name                       = "pins-fd-mds-common-tooling"
+  target_resource_id         = azurerm_cdn_frontdoor_profile.common.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.common.id
+
+  enabled_log {
+    category = "FrontdoorWebApplicationFirewallLog"
+  }
+
+  metric {
+    category = "AllMetrics"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      enabled_log,
+      metric
+    ]
+  }
+}
+
