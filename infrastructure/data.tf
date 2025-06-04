309ee9e0f730711726de57fd7ff05fe4d4e90ec4
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2025-06-04 11:42:19 +0100

    upgrade agents to ubuntu 22

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -3,7 +3,7 @@ data "azurerm_client_config" "current" {}
 data "azurerm_subscription" "current" {}
 
 data "azurerm_image" "azure_agents" {
-  name_regex          = "azure-agents-20250422143812"
+  name_regex          = "azure-agents-"
   sort_descending     = true
   resource_group_name = azurerm_resource_group.tooling.name
 }
