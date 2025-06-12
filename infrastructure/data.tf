902619d3d36b4f0a884b9f1da9416213c5ba2bf4
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-06-12 13:00:48 +0100

    chore(agents): use new image (terraform 1.12.2)

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -3,7 +3,7 @@ data "azurerm_client_config" "current" {}
 data "azurerm_subscription" "current" {}
 
 data "azurerm_image" "azure_agents" {
-  name_regex          = "azure-agents-2025-06-06-10"
+  name_regex          = "azure-agents-2025-06-12-1140"
   sort_descending     = true
   resource_group_name = azurerm_resource_group.tooling.name
 }
