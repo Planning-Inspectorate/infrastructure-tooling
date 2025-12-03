4d154169515cdd23a9868c1b0b44229a74c0c535
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2025-12-03 21:11:43 +0000

    chore(infrastructure): upgrading agents

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -3,7 +3,7 @@ data "azurerm_client_config" "current" {}
 data "azurerm_subscription" "current" {}
 
 data "azurerm_image" "azure_agents" {
-  name_regex          = "azure-agents-2025-10-08-0756"
+  name_regex          = "azure-agents-2025-12-03-1706"
   sort_descending     = true
   resource_group_name = azurerm_resource_group.tooling.name
 }
