559edd069f344f4c4fd39a828bc9cc53057b49c8
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-06-26 08:52:40 +0100

    chore(infrastructure): add VMSS for agent tests

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -12,6 +12,24 @@ locals {
     }
   }
 
+  agent_pools_test = {
+    spec_1 = {
+      name     = "pins-vmss-spec-1-${local.resource_suffix}"
+      nic_name = "pins-vnet-azure-agents-nic-spec-1-${local.resource_suffix}"
+      sku      = "Standard_D4plds_v5"
+    }
+    spec_2 = {
+      name     = "pins-vmss-spec-2-${local.resource_suffix}"
+      nic_name = "pins-vnet-azure-agents-nic-spec-2-${local.resource_suffix}"
+      sku      = "Standard_D4pds_v5"
+    }
+    spec_3 = {
+      name     = "pins-vmss-spec-3-${local.resource_suffix}"
+      nic_name = "pins-vnet-azure-agents-nic-spec-3-${local.resource_suffix}"
+      sku      = "Standard_D8plds_v5"
+    }
+  }
+
   shared_terraform_resource_group = "pins-rg-shared-terraform-uks"
 
   resource_suffix = "shared-${var.environment}-${module.azure_region_primary.location_short}"
