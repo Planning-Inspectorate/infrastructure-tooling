b6b19cbb2ec736162d86c3b86e5f94e1e228a7c3
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-07-27 15:05:00 +0100

    feat(infrastructure): creating a private endpoint for the state file

--- infrastructure/state_file_private_endpoints.tf
+++ infrastructure/state_file_private_endpoints.tf
@@ -1,21 +1,21 @@
 locals {
   state_file_storage_accounts = {
-    # "pinsstsharedtfstateuks"   = azurerm_storage_account.
-    "pinssttfstateuksappealbo" = azurerm_storage_account.appeals_back_office_terraform_storage.id
-    "pinssttfstateada"         = azurerm_storage_account.appeals_decision_assistant_terraform_storage.id
-    "pinssttfstateuksappealfo" = azurerm_storage_account.appeals_front_office_terraform_storage.id
-    "pinssttfstateappealsmigr" = azurerm_storage_account.appeals_migration_terraform_storage.id
-    "pinssttfstateuksappbo"    = azurerm_storage_account.applications_back_office_terraform_storage.id
-    "pinssttfstateuksappserfo" = azurerm_storage_account.applications_front_office_terraform_storage.id
-    "pinssttfstatecwp"         = azurerm_storage_account.casework_portal_terraform_storage.id
-    "pinssttfstateukscrowndev" = azurerm_storage_account.crown_development_terraform_storage.id
-    "pinssttfstateuksdcop"     = azurerm_storage_account.dcop_terraform_storage.id
-    "pinsstterraformdartuks"   = azurerm_storage_account.dart_terraform_storage.id
-    "pinssttfstatelocalplans"  = azurerm_storage_account.local_plans_terraform_storage.id
-    "pinssttfstateukspeas"     = azurerm_storage_account.peas_terraform_storage.id
-    "pinssttfstateukprotinfra" = azurerm_storage_account.prototypes_common_infrastructure_terraform_storage.id
-    "pinssttfstateredaction"   = azurerm_storage_account.redaction_terraform_storage.id
-    "pinssttfstateuksscheduli" = azurerm_storage_account.scheduling_terraform_storage.id
+    # "pinsstsharedtfstateuks"   = azurerm_storage_account. Terragrunt
+    # "pinssttfstateuksappealbo" = azurerm_storage_account.appeals_back_office_terraform_storage.id
+    # "pinssttfstateada"         = azurerm_storage_account.appeals_decision_assistant_terraform_storage.id
+    # "pinssttfstateuksappealfo" = azurerm_storage_account.appeals_front_office_terraform_storage.id
+    # "pinssttfstateappealsmigr" = azurerm_storage_account.appeals_migration_terraform_storage.id
+    # "pinssttfstateuksappbo"    = azurerm_storage_account.applications_back_office_terraform_storage.id
+    # "pinssttfstateuksappserfo" = azurerm_storage_account.applications_front_office_terraform_storage.id
+    # "pinssttfstatecwp"         = azurerm_storage_account.casework_portal_terraform_storage.id
+    # "pinssttfstateukscrowndev" = azurerm_storage_account.crown_development_terraform_storage.id
+    # "pinssttfstateuksdcop"     = azurerm_storage_account.dcop_terraform_storage.id
+    # "pinsstterraformdartuks"   = azurerm_storage_account.dart_terraform_storage.id
+    # "pinssttfstatelocalplans"  = azurerm_storage_account.local_plans_terraform_storage.id
+    # "pinssttfstateukspeas"     = azurerm_storage_account.peas_terraform_storage.id
+    # "pinssttfstateukprotinfra" = azurerm_storage_account.prototypes_common_infrastructure_terraform_storage.id
+    # "pinssttfstateredaction"   = azurerm_storage_account.redaction_terraform_storage.id
+    # "pinssttfstateuksscheduli" = azurerm_storage_account.scheduling_terraform_storage.id
     "pinssttfstateukstemplate" = azurerm_storage_account.template_app_terraform_storage.id
   }
 }
