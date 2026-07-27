71dabe12d08529832fcc9319a69d7f790ea93821
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-07-27 09:43:54 +0100

    feat(infrastructure): managed redis private dns zone

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -193,6 +193,17 @@ resource "azurerm_private_dns_zone_virtual_network_link" "redis" {
   virtual_network_id    = azurerm_virtual_network.tooling.id
 }
 
+resource "azurerm_private_dns_zone" "managed_redis" {
+  name                = "privatelink.redis.azure.net"
+  resource_group_name = azurerm_resource_group.tooling.name
+}
+resource "azurerm_private_dns_zone_virtual_network_link" "managed_redis" {
+  name                  = "pins-vnetlink-managed-redis-${local.resource_suffix}"
+  private_dns_zone_name = azurerm_private_dns_zone.managed_redis.name
+  resource_group_name   = azurerm_resource_group.tooling.name
+  virtual_network_id    = azurerm_virtual_network.tooling.id
+}
+
 resource "azurerm_private_dns_zone" "service_bus" {
   name                = "privatelink.servicebus.windows.net"
   resource_group_name = azurerm_resource_group.tooling.name
